13304316697d2d826ce27fdfe1868f1dc5c7c7b5
Mariana <marianapereira_f@hotmail.com>
2018-05-11 22:18:04 -0300

    Alteração frontend: Página upload de arquivos

--- vsm/views.py
+++ vsm/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
 from vsm.collection import Collection
+from .forms import UploadFileForm
+# from somewhere import handle_uploaded_file
 
 def home(request):
 
@@ -18,3 +20,33 @@ def home(request):
     template = loader.get_template('vsm/index.html')
 
     return HttpResponse(template.render(context, request))
+
+def upload(request):
+    #Codigo que funciona quando poe localhost:8000/upload
+    col = Collection("/virs/collection/")
+    tokens = col.loadCollection()
+
+    context = {
+        'section_title': 'VIRS - Visualization and Information Retrieval System',
+        'tokens' : tokens,
+    }
+
+    # load template
+    template = loader.get_template('vsm/upload.html')
+
+    return HttpResponse(template.render(context, request))
+    
+    #Do exemplo
+    # if request.method == 'POST':
+    #     form = UploadFileForm(request.POST, request.FILES)
+    #     if form.is_valid():
+    #         handle_uploaded_file(request.FILES['file'])
+    #         return HttpResponseRedirect('/success/url/')
+    # else:
+    #     form = UploadFileForm()
+    # return render(request, 'upload.html', {'form': form})
+
+    # template = loader.get_template('vsm/upload.html')
+    # return HttpResponse(template.render(request))
+
+
