6094d50c8ff3368e0cce8ed5d8c486e989c91114
Mariana <you@example.com>
2018-05-23 19:24:31 -0300

    Código similaridade arrumado.

--- cop/similarity.py
+++ cop/similarity.py
@@ -4,21 +4,21 @@ from collections import deque
 class Similarity:
 
     def sim(wd, wq):
-        tamDoc = len(wd)
-        tamQuery = len(wq)
+        doc_length = len(wd)
+        query_length = len(wq)
         
               
         numerator = 0
-        for i in range(1, tamDoc):
+        for i in range(0, doc_length):
             numerator += wd[i]*wq[i]
         
         sqrtDoc = 0
-        for i in range(1, tamDoc):
+        for i in range(0, doc_length):
             sqrtDoc += wd[i]**2
         sqrtDoc = math.sqrt(sqrtDoc)
 
         sqrtQuery = 0
-        for i in range(1, tamQuery):
+        for i in range(0, query_length):
             sqrtQuery += wq[i]**2
         sqrtQuery = math.sqrt(sqrtQuery)
 
@@ -28,8 +28,6 @@ class Similarity:
     def calculate_rank(docs, docs_tfidf, query_w):
         rank = []
         for doc, tfidf in zip(docs, docs_tfidf):
-            rank.append(sim(tfidf, query_w), doc)
+            rank.append((sim(tfidf, query_w), doc))
         rank.sort()
         return rank
-
-
